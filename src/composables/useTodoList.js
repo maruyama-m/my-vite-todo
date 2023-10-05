087ad56c4960b048499e72accc14c93ea039f717
maruyama-m <maiko.maruyama@isb.co.jp>
2023-10-05 16:39:04 +0900

    9.3分離ロジックのリファクタリング

--- src/composables/useTodoList.js
+++ src/composables/useTodoList.js
@@ -6,9 +6,66 @@ export const useTodoList = (id) => {
   const todoListRef = ref([]);
   todoListRef.value = ls ? JSON.parse(ls) : [];
 
-  const todo = todoListRef.value.find((todo) => todo.id === id);
-  const idx = todoListRef.value.findIndex((todo) => todo.id === id);
+  //追加処理
+  const add = (task) => {
+    //IDを簡易的にミリ秒で登録する
+    const id = new Date().getTime();
 
-  //retunすることでtodoとidxを外部から使うことができる
-  return { todo, idx };
+    //配列に入力TODOを格納
+    todoListRef.value.push({ id: id, task: task });
+
+    //ローカルストレージに登録
+    localStorage.todoList = JSON.stringify(todoListRef.value);
+  };
+  //retunすることで外部から使うことができる
+  return { todoListRef };
 };
+
+//編集対象となるTODOを取得
+const findByld = (id) => {
+  return todoListRef.value.find((todo) => todo.id === id);
+};
+//TODOリストから編集対象のインデックスを取得
+const findIndexByld = (id) => {
+  return todoListRef.value.findIndex((todo) => todo.id === id);
+};
+
+//編集
+const editId = ref(-1);
+const show = (id) => {
+  const todo = findByld(id);
+  editId.value = id;
+  return todo.task;
+};
+
+//変更
+const edit = (task) => {
+  const todo = findByld(editId.value);
+  const idx = findIndexByld(editId.value);
+  todo.task = task;
+  todoListRef.value.splice(idx, 1, todo);
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+  editId.value = -1;
+};
+
+//削除
+const del = (id) => {
+  const todo = findByld(id);
+  const delMsg = '「' + todo.task + '」を削除しますか？';
+  if (!confirm(delMsg)) return;
+
+  const idx = findIndexByld(id);
+  todoListRef.value.splice(idx, 1);
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+};
+
+//チェックボックスが押された時の処理
+const check = (id) => {
+  const todo = findByld(id);
+  const idx = findIndexByld(id);
+  todo.checked = !todo.checked;
+  todoListRef.value.splice(idx, 1, todo);
+  localStorage.todoList = JSON.stringify(todoListRef.value);
+};
+
+return { todoListRef, add, show, edit, del, check };
